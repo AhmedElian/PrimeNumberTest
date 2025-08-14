1512a5adb12ebb6f70ea0842bf8d0f0b7f8dc6b7
Ahmed Abdallatif <136247970+AhmedElian@users.noreply.github.com>
2025-08-14 04:37:21 +0300

    Add files via upload

--- PrimeNumberCheckerTest.java
+++ PrimeNumberCheckerTest.java
@@ -1,4 +1,5 @@
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class PrimeNumberCheckerTest {
@@ -6,7 +7,11 @@ public class PrimeNumberCheckerTest {
     @Test(dataProvider = "testData", dataProviderClass = ReadCSV.class)
     public void testPrimeNumbers(String description, int number, boolean expected) {
         PrimeNumberChecker checker = new PrimeNumberChecker();
+        Reporter.log("Starting Test With Number: " + number, true);
         boolean result = checker.isPrime(number);
+        Reporter.log("Checking If The Number Is Prime: " + expected, true);
         Assert.assertEquals(result, expected, description);
+        Reporter.log("Test Passed For Number: " + number , true);
+        System.out.println("----------------------------------------------------------------------");
     }
 }
